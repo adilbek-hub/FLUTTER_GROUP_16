f85ba60296b66b7fd8595a3e0d64fbd10ba44732
Adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-10-10 00:43:02 +0600

    done

--- sabak_7_textformfield_and_alert_dialog/lib/view/login_view.dart
+++ sabak_7_textformfield_and_alert_dialog/lib/view/login_view.dart
@@ -11,7 +11,108 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff),
+      backgroundColor: const Color(0xffe6e6e6),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 170),
+              child: Image.asset('assets/Design circle.png'),
+            ),
+            const Text(
+              'Welcome Back!',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            Image.network(
+              'https://t3.ftcdn.net/jpg/05/68/99/80/360_F_568998040_m42feFA9RajqmuR5DTlWwox44fxE3MOi.jpg',
+              width: 200,
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Material(
+                elevation: 15,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'Enter Your Email',
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Material(
+                elevation: 15,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'Enter Your Password',
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+            const Text(
+              'Forgot Password?',
+              style: TextStyle(
+                color: Color(0xff029381),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xff4ebdaf),
+                minimumSize: const Size(320, 60),
+                shape: const BeveledRectangleBorder(
+                    borderRadius: BorderRadius.zero),
+              ),
+              onPressed: () {},
+              child: const Text(
+                'Sign In',
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            const SizedBox(height: 25),
+            const Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an account?",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      color: Color(0xff4ebdaf),
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
