997a489890b3aa414b321b6d89dadb0218cd59b7
Adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-10-19 00:44:49 +0600

    do

--- sabak_8_second_page_ui/lib/view/home_view.dart
+++ sabak_8_second_page_ui/lib/view/home_view.dart
@@ -10,230 +10,243 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xffeeeeee),
-      body: Center(
-        child: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              height: 237,
-              color: const Color(0xff54B8AB),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Image.asset(
-                        'assets/Design circle.png',
-                        width: 100,
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: const Color(0xffeeeeee),
+        body: Center(
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 237,
+                color: const Color(0xff54B8AB),
+                child: Column(
+                  children: [
+                    Stack(children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Image.asset(
+                            'assets/Design circle.png',
+                            width: 100,
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            icon: const Icon(
+                              Icons.arrow_back_ios,
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                  SizedBox(
-                    width: 70,
-                    height: 70,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(100),
-                      child: Image.network(
-                        'https://karopka.ru/upload/a1d/photo_1_1475190917.jpg',
-                        fit: BoxFit.cover,
+                    ]),
+                    SizedBox(
+                      width: 70,
+                      height: 70,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(100),
+                        child: Image.network(
+                          'https://karopka.ru/upload/a1d/photo_1_1475190917.jpg',
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
-                  ),
-                  const Text(
-                    'Welcome Amelia Roe!',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24,
-                        fontWeight: FontWeight.w700),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 40),
-            Image.network(
-              'https://math-media.byjusfutureschool.com/bfs-math/2022/06/02174115/SEOBFSMSMATCON074_12%402x-300x300.png',
-              width: 70,
-            ),
-            const SizedBox(height: 32),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 18),
-              child: Container(
-                width: double.infinity,
-                height: 310,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(22),
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Color.fromARGB(137, 116, 114, 114),
-                      offset: Offset(4, 4),
-                      blurRadius: 10,
-                    )
+                    const Text(
+                      'Welcome Amelia Roe!',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.w700),
+                    ),
                   ],
                 ),
-                child: const Padding(
-                  padding:
-                      EdgeInsets.only(left: 4, top: 35, right: 52, bottom: 40),
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.check_box,
-                              size: 40,
-                            ),
-                            SizedBox(width: 7),
-                            Text(
-                              'Grocery Shoping',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 15,
-                                fontWeight: FontWeight.w700,
+              ),
+              const SizedBox(height: 40),
+              Image.network(
+                'https://math-media.byjusfutureschool.com/bfs-math/2022/06/02174115/SEOBFSMSMATCON074_12%402x-300x300.png',
+                width: 70,
+              ),
+              const SizedBox(height: 32),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 18),
+                child: Container(
+                  width: double.infinity,
+                  height: 310,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(22),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Color.fromARGB(137, 116, 114, 114),
+                        offset: Offset(4, 4),
+                        blurRadius: 10,
+                      )
+                    ],
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.only(
+                        left: 4, top: 35, right: 52, bottom: 40),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.check_box,
+                                size: 40,
                               ),
-                            ),
-                            SizedBox(width: 30),
-                            Icon(
-                              Icons.border_color,
-                              size: 40,
-                              color: Color(0xff4ebdaf),
-                            ),
-                            SizedBox(width: 10),
-                            Icon(
-                              Icons.delete,
-                              size: 40,
-                              color: Color(0xff4ebdaf),
-                            ),
-                          ],
-                        ),
-                        SizedBox(height: 8.04),
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.check_box,
-                              size: 40,
-                            ),
-                            SizedBox(width: 7),
-                            Text(
-                              'Grocery Shoping',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 15,
-                                fontWeight: FontWeight.w700,
+                              SizedBox(width: 7),
+                              Text(
+                                'Grocery Shoping',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w700,
+                                ),
                               ),
-                            ),
-                            SizedBox(width: 30),
-                            Icon(
-                              Icons.border_color,
-                              size: 40,
-                              color: Color(0xff4ebdaf),
-                            ),
-                            SizedBox(width: 10),
-                            Icon(
-                              Icons.delete,
-                              size: 40,
-                              color: Color(0xff4ebdaf),
-                            ),
-                          ],
-                        ),
-                        SizedBox(height: 8.04),
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.check_box,
-                              size: 40,
-                            ),
-                            SizedBox(width: 7),
-                            Text(
-                              'Grocery Shoping',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 15,
-                                fontWeight: FontWeight.w700,
+                              SizedBox(width: 30),
+                              Icon(
+                                Icons.border_color,
+                                size: 40,
+                                color: Color(0xff4ebdaf),
                               ),
-                            ),
-                            SizedBox(width: 30),
-                            Icon(
-                              Icons.border_color,
-                              size: 40,
-                              color: Color(0xff4ebdaf),
-                            ),
-                            SizedBox(width: 10),
-                            Icon(
-                              Icons.delete,
-                              size: 40,
-                              color: Color(0xff4ebdaf),
-                            ),
-                          ],
-                        ),
-                        SizedBox(height: 8.04),
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.check_box,
-                              size: 40,
-                            ),
-                            SizedBox(width: 7),
-                            Text(
-                              'Grocery Shoping',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 15,
-                                fontWeight: FontWeight.w700,
+                              SizedBox(width: 10),
+                              Icon(
+                                Icons.delete,
+                                size: 40,
+                                color: Color(0xff4ebdaf),
                               ),
-                            ),
-                            SizedBox(width: 30),
-                            Icon(
-                              Icons.border_color,
-                              size: 40,
-                              color: Color(0xff4ebdaf),
-                            ),
-                            SizedBox(width: 10),
-                            Icon(
-                              Icons.delete,
-                              size: 40,
-                              color: Color(0xff4ebdaf),
-                            ),
-                          ],
-                        ),
-                        SizedBox(height: 8.04),
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.check_box,
-                              size: 40,
-                            ),
-                            SizedBox(width: 7),
-                            Text(
-                              'Grocery Shoping',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 15,
-                                fontWeight: FontWeight.w700,
+                            ],
+                          ),
+                          SizedBox(height: 8.04),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.check_box,
+                                size: 40,
                               ),
-                            ),
-                            SizedBox(width: 30),
-                            Icon(
-                              Icons.border_color,
-                              size: 40,
-                              color: Color(0xff4ebdaf),
-                            ),
-                            SizedBox(width: 10),
-                            Icon(
-                              Icons.delete,
-                              size: 40,
-                              color: Color(0xff4ebdaf),
-                            ),
-                          ],
-                        ),
-                      ],
+                              SizedBox(width: 7),
+                              Text(
+                                'Grocery Shoping',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              SizedBox(width: 30),
+                              Icon(
+                                Icons.border_color,
+                                size: 40,
+                                color: Color(0xff4ebdaf),
+                              ),
+                              SizedBox(width: 10),
+                              Icon(
+                                Icons.delete,
+                                size: 40,
+                                color: Color(0xff4ebdaf),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 8.04),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.check_box,
+                                size: 40,
+                              ),
+                              SizedBox(width: 7),
+                              Text(
+                                'Grocery Shoping',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              SizedBox(width: 30),
+                              Icon(
+                                Icons.border_color,
+                                size: 40,
+                                color: Color(0xff4ebdaf),
+                              ),
+                              SizedBox(width: 10),
+                              Icon(
+                                Icons.delete,
+                                size: 40,
+                                color: Color(0xff4ebdaf),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 8.04),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.check_box,
+                                size: 40,
+                              ),
+                              SizedBox(width: 7),
+                              Text(
+                                'Grocery Shoping',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              SizedBox(width: 30),
+                              Icon(
+                                Icons.border_color,
+                                size: 40,
+                                color: Color(0xff4ebdaf),
+                              ),
+                              SizedBox(width: 10),
+                              Icon(
+                                Icons.delete,
+                                size: 40,
+                                color: Color(0xff4ebdaf),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 8.04),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.check_box,
+                                size: 40,
+                              ),
+                              SizedBox(width: 7),
+                              Text(
+                                'Grocery Shoping',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              SizedBox(width: 30),
+                              Icon(
+                                Icons.border_color,
+                                size: 40,
+                                color: Color(0xff4ebdaf),
+                              ),
+                              SizedBox(width: 10),
+                              Icon(
+                                Icons.delete,
+                                size: 40,
+                                color: Color(0xff4ebdaf),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- sabak_8_second_page_ui/lib/view/login_view.dart
+++ sabak_8_second_page_ui/lib/view/login_view.dart
@@ -9,118 +9,144 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  String email = 'adilbek@gmail.com';
+  String password = '000a';
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xffe6e6e6),
-      body: Column(
-        children: [
-          Row(
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: const Color(0xffe6e6e6),
+        body: Form(
+          key: formKey,
+          child: Column(
             children: [
-              Image.asset(
-                'assets/Design circle.png',
+              Row(
+                children: [
+                  Image.asset(
+                    'assets/Design circle.png',
+                  ),
+                ],
+              ),
+              const Text(
+                'Welcome Back!',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              Image.network(
+                'https://t3.ftcdn.net/jpg/05/68/99/80/360_F_568998040_m42feFA9RajqmuR5DTlWwox44fxE3MOi.jpg',
+                width: 200,
+              ),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Material(
+                  elevation: 15,
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Email жазыңыз';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      hintText: 'Enter Your Email',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Material(
+                  elevation: 15,
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Сиз пароль жазыыз';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      hintText: 'Enter Your Password',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              const Text(
+                'Forgot Password?',
+                style: TextStyle(
+                  color: Color(0xff029381),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xff4ebdaf),
+                  minimumSize: const Size(320, 60),
+                  shape: const BeveledRectangleBorder(
+                      borderRadius: BorderRadius.zero),
+                ),
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const HomeView(),
+                      ),
+                    );
+                  }
+                },
+                child: const Text(
+                  'Sign In',
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 25),
+              const Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't have an account?",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        color: Color(0xff4ebdaf),
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
-          const Text(
-            'Welcome Back!',
-            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 20),
-          Image.network(
-            'https://t3.ftcdn.net/jpg/05/68/99/80/360_F_568998040_m42feFA9RajqmuR5DTlWwox44fxE3MOi.jpg',
-            width: 200,
-          ),
-          const SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Material(
-              elevation: 15,
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Enter Your Email',
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Material(
-              elevation: 15,
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Enter Your Password',
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(height: 30),
-          const Text(
-            'Forgot Password?',
-            style: TextStyle(
-              color: Color(0xff029381),
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(height: 30),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: const Color(0xff4ebdaf),
-              minimumSize: const Size(320, 60),
-              shape:
-                  const BeveledRectangleBorder(borderRadius: BorderRadius.zero),
-            ),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (contex) => const HomeView(),
-                ),
-              );
-            },
-            child: const Text(
-              'Sign In',
-              style: TextStyle(
-                fontSize: 22,
-                color: Colors.white,
-              ),
-            ),
-          ),
-          const SizedBox(height: 25),
-          const Center(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Don't have an account?",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                SizedBox(width: 10),
-                Text(
-                  'Sign Up',
-                  style: TextStyle(
-                    color: Color(0xff4ebdaf),
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
