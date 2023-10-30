8b98c0b09d55ac354646e8b82b4773c81f9b398b
Adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-10-30 23:40:22 +0600

    dice app full done

--- sabak_9_dice_app/lib/views/home_view.dart
+++ sabak_9_dice_app/lib/views/home_view.dart
@@ -12,48 +12,87 @@ class MyHomeView extends StatefulWidget {
 
 class _MyHomeViewState extends State<MyHomeView> {
   Random random = Random();
-  int firstDice = 1;
-  int secondDice = 1;
-  int sum = 0;
+  int firstNumDice = 1;
+  int secondNumDice = 1;
+  int sum1 = 0;
+  String firstGammer = 'АСАН';
 
-  void changeDices() {
-    // firstDice  өзгөрмөсүнө рандомду байлоо
-    firstDice = random.nextInt(6) + 1;
+  void firstChangeDices() {
+    // firstNumDice  өзгөрмөсүнө рандомду байлоо
+    firstNumDice = random.nextInt(6) + 1;
     // secondDice  өзгөрмөсүнө рандомду байлоо
-    secondDice = random.nextInt(6) + 1;
-    //Абалдын өзгөрүүсү үчүн setState методун колдонуу.
+    secondNumDice = random.nextInt(6) + 1;
     // Чыккан санды кошуу
-    sum = sum + firstDice + secondDice;
-
+    sum1 = sum1 + firstNumDice + secondNumDice;
+    //Абалдын өзгөрүүсү үчүн setState методун колдонуу.
     setState(() {});
+    result();
   }
 
-  Future<void> _showMyDialog() async {
-    return showDialog<void>(
-      context: context,
-      barrierDismissible: false, // user must tap button!
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('AlertDialog Title'),
-          content: const SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                Text('This is a demo alert dialog.'),
-                Text('Would you like to approve of this message?'),
+  int thirdNumDice = 1;
+  int fourthNumDice = 1;
+  int sum2 = 0;
+  String secondGammer = 'ҮСӨН';
+
+  void secondChangeDices() {
+    // firstNumDice  өзгөрмөсүнө рандомду байлоо
+    thirdNumDice = random.nextInt(6) + 1;
+    // secondDice  өзгөрмөсүнө рандомду байлоо
+    fourthNumDice = random.nextInt(6) + 1;
+    // Чыккан санды кошуу
+    sum2 = sum2 + thirdNumDice + fourthNumDice;
+    //Абалдын өзгөрүүсү үчүн setState методун колдонуу.
+    setState(() {});
+    result();
+  }
+
+  void result() {
+    if (sum1 >= 50) {
+      showDialog(
+          context: context,
+          builder: (contex) {
+            return AlertDialog(
+              title: Text('$firstGammer ЧЕМПИОН'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    resetAll();
+                    Navigator.pop(context);
+                    setState(() {});
+                  },
+                  child: const Text('Чыгуу'),
+                ),
               ],
-            ),
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: const Text('Approve'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
+            );
+          });
+    } else if (sum2 >= 50) {
+      showDialog(
+          context: context,
+          builder: (contex) {
+            return AlertDialog(
+              title: Text('$secondGammer ЧЕМПИОН'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    resetAll();
+                    Navigator.pop(context);
+                    setState(() {});
+                  },
+                  child: const Text('Чыгуу'),
+                ),
+              ],
+            );
+          });
+    }
+  }
+
+  void resetAll() {
+    firstNumDice = 1;
+    secondNumDice = 1;
+    sum1 = 0;
+    thirdNumDice = 1;
+    fourthNumDice = 1;
+    sum2 = 0;
   }
 
   @override
@@ -70,9 +109,17 @@ class _MyHomeViewState extends State<MyHomeView> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          if (sum >= 50) Text('Сиздин эсебиңиз $sum'),
           Text(
-            '$sum',
+            firstGammer,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            '$sum1',
             style: const TextStyle(
               fontSize: 30,
               fontWeight: FontWeight.bold,
@@ -85,22 +132,64 @@ class _MyHomeViewState extends State<MyHomeView> {
                   height: 200,
                   child: InkWell(
                       onTap: () {
-                        changeDices();
-                        // result();
+                        firstChangeDices();
                       },
-                      child:
-                          SvgPicture.asset('assets/die_face_$firstDice.svg'))),
+                      child: SvgPicture.asset(
+                          'assets/die_face_$firstNumDice.svg'))),
               SizedBox(
                   width: 200,
                   height: 200,
                   child: InkWell(
                       onTap: () {
-                        changeDices();
-                        // result();
+                        firstChangeDices();
                       },
-                      child:
-                          SvgPicture.asset('assets/die_face_$secondDice.svg'))),
+                      child: SvgPicture.asset(
+                          'assets/die_face_$secondNumDice.svg'))),
             ],
+          ),
+          const Divider(
+            color: Colors.black,
+            indent: 20,
+            endIndent: 20,
+          ),
+          const SizedBox(height: 20),
+          Row(
+            children: [
+              SizedBox(
+                  width: 200,
+                  height: 200,
+                  child: InkWell(
+                      onTap: () {
+                        secondChangeDices();
+                      },
+                      child: SvgPicture.asset(
+                          'assets/die_face_$thirdNumDice.svg'))),
+              SizedBox(
+                  width: 200,
+                  height: 200,
+                  child: InkWell(
+                      onTap: () {
+                        secondChangeDices();
+                      },
+                      child: SvgPicture.asset(
+                          'assets/die_face_$fourthNumDice.svg'))),
+            ],
+          ),
+          Text(
+            '$sum2',
+            style: const TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            secondGammer,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ],
       ),
